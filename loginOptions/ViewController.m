9c5e3fe17d31bd3a33faed051925cc3b5a4f8db6
Gabriel Massana <Gabi@LOCK8s-Mac-mini.local>
2014-07-21 14:30:26 +0100

    Initial Commit

---
+++ loginOptions/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  loginOptions
+//
+//  Created by Gabriel Massana on 21/07/2014.
+//  Copyright (c) 2014 Gabriel Massana. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ loginOptions/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  loginOptions
+//
+//  Created by Gabriel Massana on 21/07/2014.
+//  Copyright (c) 2014 Gabriel Massana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
